19c74982af244007eb1cb358dc8f764c8615754e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-01 14:31:53 -0700

    Update user types with annotation

--- code/api/src/modules/user/types.js
+++ code/api/src/modules/user/types.js
@@ -12,6 +12,9 @@ const UserType = new GraphQLObjectType({
     email: { type: GraphQLString },
     password: { type: GraphQLString },
     role: { type: GraphQLString },
+    // And maybe something like this:
+    
+    // personal_style: { type: GraphQLString },
     createdAt: { type: GraphQLString },
     updatedAt: { type: GraphQLString }
   })
