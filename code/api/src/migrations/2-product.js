6dc15cbaa3d078c92a55510f82f9516849ad4c77
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-12-07 08:46:18 -0600

    Adds annotation and code ideas for project within product.js

--- code/api/src/migrations/2-product.js
+++ code/api/src/migrations/2-product.js
@@ -19,6 +19,9 @@ module.exports = {
       type: {
         type: Sequelize.INTEGER
       },
+      // styles: {
+      // {...style: Sequalize.STRING}
+      // }
       gender: {
         type: Sequelize.INTEGER
       },
@@ -39,3 +42,9 @@ module.exports = {
     return queryInterface.dropTable('products');
   }
 }
+
+// This looks like the most strategic place to add the style update.  I do wonder if one product can have multiple styles, which is why I believe it should be an array of sorts.
+
+// Here's a doc I found that might help us figure out how to list multiple styles: https://typeofnan.dev/an-approach-to-js-object-schema-migration/
+
+// id is an integer, name is a string, slug is a string (and this is used to develop additional info in a URI that matches with the name, this is processed autmatically with code that is available in the codebase), description is text, type is integer, styles I would want it to be an array, but right now we are accepting that it can be a string, gender is an integer, image is in text, createdAt/updatedAt as a time object and it is NOT allowed to be null.
