8cf698a182b7d3d1cb282bb6d7d72c6190d349ad
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-12-07 09:24:34 -0600

    Adds annotations in model.js for the module project

--- code/api/src/modules/product/model.js
+++ code/api/src/modules/product/model.js
@@ -15,6 +15,9 @@ module.exports = function(sequelize, DataTypes) {
     type: {
       type: DataTypes.INTEGER
     },
+    // styleType {
+    // type: DataTypes.INTEGER
+    // },
     gender: {
       type: DataTypes.INTEGER
     },
@@ -23,3 +26,16 @@ module.exports = function(sequelize, DataTypes) {
     }
   })
 }
+
+// If we add styleType as an array, we'll have to change that here.  But since I have no idea how to create an array in Javascript (YET!) we are going with an integer that represents different styles.
+
+// This model has the following queries:
+// getAll
+// getBySlug
+// getById
+// getRelated
+// getTypes
+
+// we will be adding a getStyle within the query and resolvers.
+
+// This model holds a name as string, slug as string, description as text, type as integer, styleType as integer we will add, gender as an integer, image as text.
