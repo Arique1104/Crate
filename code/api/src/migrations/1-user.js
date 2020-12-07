7f57164874bb41f2301fe81385b8bb90886a0a31
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-12-07 08:33:30 -0600

    Adds code understanding and annotation to user.js

--- code/api/src/migrations/1-user.js
+++ code/api/src/migrations/1-user.js
@@ -33,3 +33,7 @@ module.exports = {
     return queryInterface.dropTable('users');
   }
 }
+//This document is setting the user and giving the database all it's desired attributes.
+
+// it has:
+//id as an integer, name as a string, email as text, password as text, role as text, and createdAt/updatedAt as a time object and it is NOT allowed to be null.
