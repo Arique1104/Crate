e7bf35d3731a6362f48516c2ac605fc02384513f
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-12-07 08:50:54 -0600

    Adds annotations to crates.js

--- code/api/src/migrations/3-crates.js
+++ code/api/src/migrations/3-crates.js
@@ -27,3 +27,9 @@ module.exports = {
     return queryInterface.dropTable('crates');
   }
 }
+
+// I still believe we should have multiple crates that are representative of multiple styles so that the products can be present in multiple places that they fit in.  But I'm not sure how we would go about that.  It could be a graphql request that searches all product style arrays that match the style ids represented in the crates.
+
+// But as for now, we are leaving these crates alone.
+
+// Crates have an id as an integer, a name as a string, a description as text, and createdAt/updatedAt as a time object and it is NOT allowed to be null.
