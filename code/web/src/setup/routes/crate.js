4a48a06e831524a40b85d32eda4e529a322075c3
Hanna Kim <hannakim91@gmail.com>
2020-12-06 18:20:09 -0700

    Annotate where feature should start

--- code/web/src/setup/routes/crate.js
+++ code/web/src/setup/routes/crate.js
@@ -1,7 +1,8 @@
 // App Imports
 import List from '../../modules/crate/List'
 
-// Crate routes
+// Crate routes - path where new feature building starts
+  // when user clicks 'subscriptions' user should be routed to /crates and see subscription options 
 export default {
   list: {
     path: '/crates',
