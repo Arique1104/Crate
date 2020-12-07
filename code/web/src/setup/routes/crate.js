31a501cb25f37bc20882d45b148265526f90d675
Brigette Doelp <brigette.doelp@gmail.com>
2020-12-07 12:32:57 -0700

    Add annotation for route on crates area

--- code/web/src/setup/routes/crate.js
+++ code/web/src/setup/routes/crate.js
@@ -2,6 +2,8 @@
 import List from '../../modules/crate/List'
 
 // Crate routes
+  //this is where the whole thing should start! at this route is where all
+  //the crate sub options live and when one is selected it'll take ya to a style survey
 export default {
   list: {
     path: '/crates',
