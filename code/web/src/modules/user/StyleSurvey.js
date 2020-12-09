83a8027d47a0b6f62bdbbf6d1e9440d0046e07d1
Brigette Doelp <brigette.doelp@gmail.com>
2020-12-08 17:47:06 -0700

    Add comments for the StyleSurvey skeleton

--- code/web/src/modules/user/StyleSurvey.js
+++ code/web/src/modules/user/StyleSurvey.js
@@ -1,21 +1,62 @@
 //Possible Needed Imports (basing this on other files pertaining to user)
 
-////Seems like these are pretty consistent across all user files.
-
+//////Seems like these are pretty consistent across all user files.
 // import React from 'react'
 // import PropTypes from 'prop-types'
 // import { connect } from 'react-redux'
 // import { Helmet } from 'react-helmet'
 // import { Link } from 'react-router-dom'
 
-////UI stuff we'll probably need
+//////UI stuff we'll probably need
 // import { H3, H4 } from '../../ui/typography'
 // import Button from '../../ui/button'
 // import { grey, grey2 } from '../../ui/common/colors'
 
-////ETC stuff we'll probably need
+//////ETC stuff we'll probably need
 //import userRoutes from '../../setup/routes/user'
-////^^regarding above, i think we'll need to put the route back into the big 'ol file with the rest of 'em
+//////^^regarding above, i think we'll need to put the route back into the big 'ol file with the rest of 'em
 //import AuthCheck from '../auth/AuthCheck'
 //import { APP_URL } from '../../setup/config/env'
-////^^literally have no idea if we'll need this but it seems important???
+//////^^literally have no idea if we'll need this but it seems important???
+
+
+//render skeleton!!! i have some Thots
+
+render() {
+  return (
+    <div>
+      <form>
+        <Grid>
+          <GridCell>
+            {/*this is where the images and such go! they will be in a grid. we'll have 3 each.*/}
+          </GridCell>
+          <GridCell>
+            {/*this is where the images and such go! they will be in a grid. we'll have 3 each.*/}
+          </GridCell>
+          <GridCell>
+            {/*this is where the images and such go! they will be in a grid. we'll have 3 each.*/}
+          </GridCell>
+        </Grid>
+        {/*^^ Repeat the grid and gridcell pattern for each element of the survey*/}
+        <Button></Button>
+      </form>
+      {/*so, Thots about this, we've got some options when it comes to recycling styling junk
+      thats already here. FOR EXAMPLE;
+
+      <Grid></Grid>
+
+      thinking that can go INTO the form to grid out the options a user can pick for style options
+      for each category? might be nifty. same goes for this guy;
+
+      <GridCell></GridCell>
+
+      just seems like an easy way to format that. after that, it's possible that we'll wanna use
+      the <Loading/> fella. not sure tho? idk might be nice for sad path stuff.*/}
+    </div>
+
+
+
+
+
+  )
+}
