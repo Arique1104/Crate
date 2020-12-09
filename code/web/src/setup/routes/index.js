78deafaa13a73822699af9ca90c3d4c88f1e1f80
Hanna Kim <hannakim91@gmail.com>
2020-12-09 15:54:30 -0700

    Add note to routes/index.js

--- code/web/src/setup/routes/index.js
+++ code/web/src/setup/routes/index.js
@@ -6,7 +6,7 @@ import user from './user'
 import product from './product'
 import crate from './crate'
 
-// Combined routes
+// Combined routes - add any new routes to this
 export const routes = Object.assign(admin, home, user, product, crate)
 
 // API Routes
