5039be3e04bafd6f1a74da8a3b2d67a968c9d0b4
Tim Keresey <tim.keresey@gmail.com>
2020-12-10 14:06:28 -0700

    pass initial List unit test

--- code/web/src/modules/crate/List.test.js
+++ code/web/src/modules/crate/List.test.js
@@ -6,29 +6,30 @@ import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from 'react-router-dom';
 import { createStore } from 'redux';
-import rootReducer from '../../setup/store.js';
+import { store }from '../../setup/store.js';
 import configureStore from 'redux-mock-store';
 import List from './List.js';
 
-const mockStore = configureStore([]);
+// const mockStore = configureStore([]);
 
 describe('List', () => {
 
   it('should render the List component', () =>{
-    const rootReducer = jest.fn();
-    let store = mockStore({
-      isLoading: false,
-      error: null,
-      list: []
-    })
-    render (
+    // const rootReducer = jest.fn();
+    // const store = createStore(Reducer);
+    // ({
+    //   isLoading: false,
+    //   error: null,
+    //   list: []
+    // })
+    const { getByText } = render (
       <Provider store={store}>
         <BrowserRouter>
           <List />
         </BrowserRouter>
       </Provider>
     );
-    screen.debug();
-    expect(screen.getByText('Crates for everyone!')).toBeInTheDocument();
+    // screen.debug();
+    expect(getByText('Crates for everyone!')).toBeInTheDocument();
   })
 })
