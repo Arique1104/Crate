7412e05cd77a1a182642ffa8c4d808a338eb75a2
Hanna Kim <hannakim91@gmail.com>
2020-12-10 19:00:26 -0700

    Create mocks directory and add mock axios

--- code/web/src/modules/crate/api/state.test.js
+++ code/web/src/modules/crate/api/state.test.js
@@ -17,7 +17,7 @@ describe('crates reducer', () => {
     expect(result).toEqual(cratesInitialState)
   });
 
-  it('should change isLoading to true when CRATES_GET_LSIT_REQUEST action is passed', () => {
+  it('should change isLoading to true when CRATES_GET_LIST_REQUEST action is passed', () => {
     const getListRequestAction = {
       type: CRATES_GET_LIST_REQUEST,
       error: null,
